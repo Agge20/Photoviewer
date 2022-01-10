6c073ca1c740934ba2e3613b044178d634fe9d18
August <augustraro112@hotmail.com>
2022-01-10 01:00:56 +0100

    Changed the layout in the template

--- src/views/Album.jsx
+++ src/views/Album.jsx
@@ -131,23 +131,16 @@ const Album = () => {
                 disabled={loading}
               />
             </label>
-            <div>
-              <button
-                className="bg-rose-600 btn-primary"
-                onClick={() => onDelete()}
-              >
-                Delete Album
-              </button>
-            </div>
+
             {/* create new album in album */}
             <CreateAlbumFromImgs images={newAlbumImages} />
             {imageUploadErr && <span>{imageUploadErr}</span>}
           </div>
         )}
       </div>
-      <div className="mb-4">
+      <div className="mb-8 flex">
         <button
-          className="btn-warning flex"
+          className="btn-warning--yellow flex"
           onClick={() => setNewAlbumImages([])}
         >
           Clear Selected{" "}
@@ -165,6 +158,12 @@ const Album = () => {
               d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
             />
           </svg>
+        </button>
+        <button
+          className="bg-rose-600 btn-primary flex ml-2"
+          onClick={() => onDelete()}
+        >
+          Delete Album <TrashcanWhite />
         </button>
       </div>
       <div className="w-full">
